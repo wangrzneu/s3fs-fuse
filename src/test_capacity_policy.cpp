b8946b167b5666b9ac00882a8ed788e210514f7d
wangrzneu <wangrzneu@gmail.com>
2026-04-09 17:07:13 +0800

    test: fix uint64 assertion type portability on linux

--- src/test_capacity_policy.cpp
+++ src/test_capacity_policy.cpp
@@ -21,6 +21,7 @@ static constexpr uint64_t ONE_GIB = 1024ULL * 1024ULL * 1024ULL;
 static constexpr uint64_t TWO_TIB = 2ULL * ONE_TIB;
 static constexpr uint64_t THREE_TIB = 3ULL * ONE_TIB;
 static constexpr uint64_t BLOCK_SZ = 16ULL * 1024ULL * 1024ULL;
+static constexpr uint64_t STATFS_BLOCK_SZ = 4096ULL;
 
 void test_parse_capacity_mode()
 {
@@ -48,12 +49,13 @@ void test_legacy_mode()
 {
     const uint64_t bucket_blocks = 12345;
     const CapacityResult result = ComputeCapacity(CapacityMode::Legacy, bucket_blocks, 123, 456, 4096);
+    const uint64_t expected_bytes = bucket_blocks * STATFS_BLOCK_SZ;
 
     ASSERT_EQUALS(bucket_blocks, result.f_blocks);
     ASSERT_EQUALS(bucket_blocks, result.f_bfree);
     ASSERT_EQUALS(bucket_blocks, result.f_bavail);
-    ASSERT_EQUALS(bucket_blocks * 4096ULL, result.total_bytes);
-    ASSERT_EQUALS(bucket_blocks * 4096ULL, result.free_bytes);
+    ASSERT_EQUALS(expected_bytes, result.total_bytes);
+    ASSERT_EQUALS(expected_bytes, result.free_bytes);
 }
 
 void test_redis_mode_default_size()
@@ -69,12 +71,13 @@ void test_redis_mode_default_size()
 
 void test_redis_mode_used_capacity()
 {
-    const CapacityResult result = ComputeCapacity(CapacityMode::Redis, 0, ONE_TIB, 512ULL * ONE_GIB, BLOCK_SZ);
+    const uint64_t half_tib = uint64_t{512} * ONE_GIB;
+    const CapacityResult result = ComputeCapacity(CapacityMode::Redis, 0, ONE_TIB, half_tib, BLOCK_SZ);
 
     ASSERT_EQUALS(ONE_TIB, result.total_bytes);
-    ASSERT_EQUALS(512ULL * ONE_GIB, result.free_bytes);
+    ASSERT_EQUALS(half_tib, result.free_bytes);
     ASSERT_EQUALS(ONE_TIB / BLOCK_SZ, result.f_blocks);
-    ASSERT_EQUALS((512ULL * ONE_GIB) / BLOCK_SZ, result.f_bfree);
+    ASSERT_EQUALS(half_tib / BLOCK_SZ, result.f_bfree);
     ASSERT_EQUALS(result.f_bfree, result.f_bavail);
 }
 

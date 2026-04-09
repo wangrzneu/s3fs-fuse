16842495f2e41d268227dbe16ae1e1f5aa724894
wangrzneu <wangrzneu@gmail.com>
2026-04-09 16:07:15 +0800

    feat: add metadata backend interface and null backend factory

---
+++ src/test_metadata_backend_factory.cpp
@@ -0,0 +1,70 @@
+/*
+ * s3fs - FUSE-based file system backed by Amazon S3
+ *
+ * Copyright(C) 2026
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <cstdio>
+#include <string>
+
+#include "metadata_backend_factory.h"
+#include "test_util.h"
+
+namespace {
+
+void test_empty_redis_uri_returns_null_backend()
+{
+    MetadataBackendConfig config;
+    MetadataBackendPtr backend = CreateMetadataBackend(config);
+
+    ASSERT_TRUE(backend != nullptr);
+    ASSERT_FALSE(backend->GetName().empty());
+    ASSERT_STREQUALS("null", backend->GetName().c_str());
+}
+
+void test_redis_uri_still_returns_null_backend()
+{
+    MetadataBackendConfig config;
+    config.redis_uri = "redis://127.0.0.1:6379/0";
+    MetadataBackendPtr backend = CreateMetadataBackend(config);
+
+    ASSERT_TRUE(backend != nullptr);
+    ASSERT_FALSE(backend->GetName().empty());
+    ASSERT_STREQUALS("null", backend->GetName().c_str());
+}
+
+} // namespace
+
+int main(int argc, const char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    test_empty_redis_uri_returns_null_backend();
+    test_redis_uri_still_returns_null_backend();
+    std::puts("test_metadata_backend_factory: OK");
+    return 0;
+}
+
+/*
+* Local variables:
+* tab-width: 4
+* c-basic-offset: 4
+* End:
+* vim600: expandtab sw=4 ts=4 fdm=marker
+* vim<600: expandtab sw=4 ts=4
+*/
